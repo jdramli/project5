de85a4bb728568af38fd2565cb07cbe70fb6b7cf
Joseph Ramli <jramli1@binghamton.edu>
2019-10-30 23:28:23 -0400

    Opening Commit, cleared out most default code and added SKShapeNode circle and 'Bullet' class file to begin framework

---
+++ project5/project5/GameScene.swift
@@ -0,0 +1,80 @@
+//
+//  GameScene.swift
+//  project5
+//
+//  Created by Joseph Daniel Ramli on 10/30/19.
+//  Copyright © 2019 Joseph Daniel Ramli. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    //variables added from default
+    var bullet = SKShapeNode(circleOfRadius: 10)
+    
+    override func sceneDidLoad() {
+
+        self.lastUpdateTime = 0
+        bullet.position = CGPoint(x: frame.midX, y: frame.midY)
+        bullet.strokeColor = SKColor.white
+        bullet.fillColor = SKColor.white
+        self.addChild(bullet)
+    
+       
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+       
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
+    }
+}
---
+++ project5/project5/bullet.swift
@@ -0,0 +1,13 @@
+//
+//  bullet.swift
+//  project5
+//
+//  Created by Joseph Daniel Ramli on 10/30/19.
+//  Copyright © 2019 Joseph Daniel Ramli. All rights reserved.
+//
+
+import Foundation
+
+class Bullet{
+    
+}
