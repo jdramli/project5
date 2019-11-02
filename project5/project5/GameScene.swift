3cf2fe3596871a374855468f6971bcadaba1e0a7
Joseph Ramli <jramli1@binghamton.edu>
2019-11-02 00:23:34 -0400

    Bullets fly from SKSprite node now with manual offset toward the shooter.  can revisit workaround for better code later

--- project5/project5/GameScene.swift
+++ project5/project5/GameScene.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Joseph Daniel Ramli. All rights reserved.
 //
 
+import CoreGraphics
 import SpriteKit
 import GameplayKit
 
@@ -16,25 +17,38 @@ class GameScene: SKScene {
     
     private var lastUpdateTime : TimeInterval = 0
     //variables added from default
+    private var picbullet : SKSpriteNode?
     private var bullet : SKShapeNode?
     private var hero : SKShapeNode?
+    private var player : SKSpriteNode?
     
     override func sceneDidLoad() {
 
         self.lastUpdateTime = 0
         //Can this Section be shifted to a class file? This might help segregate out modifying hero parameters and interactions
+        /*
+        //This section of code is broken, I think because of improperly moving a CGRect via setting the position to a CGPoint.  I think it should be somethine instead like: "let moveRect = SKAction.moveTo(x:100, y:100)"; hero.runAction(moveRect)
         self.hero = SKShapeNode.init(rect: (CGRect(x: frame.minX+100, y:frame.minY+100, width: 70, height: 20)))
         self.hero?.strokeColor = SKColor.yellow
         self.hero?.fillColor = SKColor.yellow
         self.addChild(hero!)
         hero!.position = CGPoint(x:100, y: 100)
+         */
+        
         //Can this Section be shifted to a class file? This might help segregate out modifying bullet parameters and interactions
+        self.picbullet = self.childNode(withName: "//bullet") as? SKSpriteNode
+        //Adding the option of a shape bullet or a sprite bullet by building both for now
         self.bullet = SKShapeNode.init(circleOfRadius: 10)
         self.bullet?.position = CGPoint(x: frame.midX, y: frame.midY)
         self.bullet?.strokeColor = SKColor.white
         self.bullet?.fillColor = SKColor.white
         self.addChild(bullet!)
+      
         
+        
+        self.player = self.childNode(withName: "//hero") as? SKSpriteNode
+        let moveRect = SKAction.moveTo(x: 74.0, duration: 0)
+        player!.run(moveRect)
         
         //make a path for the white circle to move
         
@@ -45,25 +59,29 @@ class GameScene: SKScene {
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.bullet?.copy() as! SKShapeNode? {
+        //if let n = self.picbullet?.copy() as! SKSpriteNode? {//uncomment this for picbullet
+        if let n = self.bullet?.copy() as! SKShapeNode? { //uncomment this for shape bullet
             //n.position = pos
             //n.position = CGPoint(x:-100, y: -100)
-            n.position = CGPoint(x: hero!.position.x, y: hero!.position.y)
+            n.position = CGPoint(x: player!.position.x-30, y: player!.position.y+150)
             //CRITICAL DEBUGGING ISSUE ENCOUNTERED -- SETTING THIS POSITION TO 0,0 USES THE ORIGINAL SCREEN 0,0 WHICH IS THE MIDDLE.  HOWEVER, THE 0,0 OF AN SKSHAPENODE IS THE BOTTOM-LEFT CORNER OF THE SCREEN.  NEED TO FIND A WAY TO LINK THESE TWO TO THE SAME POINT OF REFERENCE!!!
-            
+            print(player!.position.x,player!.position.y)
+            print(n.position.x,n.position.y)
+            print(player!.size)
+            print(player!.centerRect)
             n.strokeColor = SKColor.green
             n.fillColor = SKColor.green
+            
             self.addChild(n)
             
-            hero?.position = CGPoint(x:0,y:0) //IN SPRITEKIT, THE 0,0 POSITION IS THE BOTTOM LEFT CORNER OF THE SCREEN, NOT THE MIDDLE
+            //hero?.position = CGPoint(x:0,y:0) //IN SPRITEKIT, THE 0,0 POSITION IS THE BOTTOM LEFT CORNER OF THE SCREEN, NOT THE MIDDLE
             
             //hero?.position = CGPoint(x:Double.random(in:-100...100), y:Double.random(in:-100...100))
             
             //This section of code creates a path object and makes n move along the path
             let path = UIBezierPath()
-            
-            //print("hero position x: ", hero!.position.x, "and y: ", hero!.position.y)
-            path.move(to: CGPoint(x:hero!.position.x,y:hero!.position.y))
+            //path.move(to: CGPoint(x:hero!.position.x,y:hero!.position.y))
+            path.move(to: CGPoint(x:player!.position.x,y:player!.position.y))
             path.addLine(to: CGPoint(x:0,y:200))
             let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: true, speed: 200)
             n.run(move)
