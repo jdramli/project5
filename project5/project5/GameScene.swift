86b266a3d6c13086f6ab0a35801afb5eb862d829
Joseph Ramli <jramli1@binghamton.edu>
2019-11-02 22:32:13 -0400

    Added enemy class and a movement path

--- project5/project5/GameScene.swift
+++ project5/project5/GameScene.swift
@@ -17,10 +17,13 @@ class GameScene: SKScene {
     
     private var lastUpdateTime : TimeInterval = 0
     //variables added from default
-    private var picbullet : SKSpriteNode?
+    private var picbullet : SKSpriteNode? //Opted not to use this for now as of 11/3/19
+    private var hero : SKShapeNode? //Opted not to use this for now as of 11/3/19
+    
     private var bullet : SKShapeNode?
-    private var hero : SKShapeNode?
     private var player : SKSpriteNode?
+    private var enemy : SKSpriteNode?
+    private var gameTimer: Timer? //Timer object to be called regularly
     
     override func sceneDidLoad() {
 
@@ -47,52 +50,66 @@ class GameScene: SKScene {
         
         
         self.player = self.childNode(withName: "//hero") as? SKSpriteNode
-        let moveRect = SKAction.moveTo(x: 74.0, duration: 0)
-        player!.run(moveRect)
+        self.enemy = self.childNode(withName: "//enemy") as? SKSpriteNode
+        //let moveRect = SKAction.moveTo(x: 74.0, duration: 0) //This was just a test line
+        //player!.run(moveRect) //This was just a test line
         
         //make a path for the white circle to move
         
+        //MyNotes; Create a timer cycle to generate the enemy objects every few seconds
+        gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
         
     
        
     }
     
+    @objc func runTimedCode(){
+        if let e = self.enemy?.copy() as! SKSpriteNode?{
+            e.position = player!.anchorPoint
+            self.addChild(e)
+            let enemy_path = UIBezierPath()
+            
+            enemy_path.move(to: CGPoint(x: Double.random(in:-300...300), y: 590))
+            //path.addLine(to: CGPoint(x:0,y:200)) //bullet shoots toward center.
+            enemy_path.addLine(to: CGPoint(x:player!.position.x,y:player!.position.y-1500) ) //Added 1500 because that is a significant offscreen distance for the bullet to travel.
+            let move = SKAction.follow(enemy_path.cgPath, asOffset: true, orientToPath: true, speed: 200)
+            let sequence = SKAction.sequence([move, .removeFromParent()]) //This "sequence" is critical because it removes the bullet from parent with the function ".removeFromParent()" once the "move" function is complete
+            e.run(sequence)
+        }
+    }
     
     func touchDown(atPoint pos : CGPoint) {
         
-        //First, move player, then create shot. I will use the click to create a moveTo function here for the player and the bullet.  Perhaps other mechanics can be explored for moving the player later, but for now I will make do with this since it is a mobile game.
-        player!.position = pos
+        //MyNotes: First, move player, then create shot. The moveTo function proved to be a bit tricky/buggy, so I implemented the quick fix below of fixing the y position.
+        player!.position = CGPoint(x: pos.x, y: -593) //Quick fix to make the player move just along the "x" axis of the touchDown pos by setting y to a fixed 593 and the x to the input of pos.x.
+        
+        //MyNotes: Next, make a copy of the bullet-sprite node for manipulation
         
         //if let n = self.picbullet?.copy() as! SKSpriteNode? {//uncomment this for picbullet
         if let n = self.bullet?.copy() as! SKShapeNode? { //uncomment this for shape bullet
-                   n.position = player!.anchorPoint //TODO: CRITICAL NOTE, MUST USE "anchorPoint" to generate proper player position when launching a sprite node from the player's "position"
-                  //n.position = CGPoint(x:-100, y: -100)
-                  //n.position = CGPoint(x: player!.position.x-30, y: player!.position.y+150)
-                  //CRITICAL DEBUGGING ISSUE ENCOUNTERED -- SETTING THIS POSITION TO 0,0 USES THE ORIGINAL SCREEN 0,0 WHICH IS THE MIDDLE.  HOWEVER, THE 0,0 OF AN SKSHAPENODE IS THE BOTTOM-LEFT CORNER OF THE SCREEN.  NEED TO FIND A WAY TO LINK THESE TWO TO THE SAME POINT OF REFERENCE!!!
-                  print(player!.position.x,player!.position.y)
-                  print(n.position.x,n.position.y)
-                  print(player!.size)
-                  print(player!.centerRect)
-                  n.strokeColor = SKColor.green
-                  n.fillColor = SKColor.green
-                  
-                  self.addChild(n)
-                  
-                  //hero?.position = CGPoint(x:0,y:0) //IN SPRITEKIT, THE 0,0 POSITION IS THE BOTTOM LEFT CORNER OF THE SCREEN, NOT THE MIDDLE
-                  
-                  //hero?.position = CGPoint(x:Double.random(in:-100...100), y:Double.random(in:-100...100))
-                  
-                  //This section of code creates a path object and makes n move along the path
-                  let bulletpath = UIBezierPath()
-                  //path.move(to: CGPoint(x:hero!.position.x,y:hero!.position.y))
-                  //bulletpath.move(to: CGPoint(x:player!.position.x,y:player!.position.y))
-                   bulletpath.move(to: player!.position)
-                  //path.addLine(to: CGPoint(x:0,y:200)) //bullet shoots toward center.
-                  bulletpath.addLine(to: CGPoint(x:player!.position.x,y:player!.position.y+1500) ) //Added 1500 because that is a significant offscreen distance for the bullet to travel.
-                  let move = SKAction.follow(bulletpath.cgPath, asOffset: true, orientToPath: true, speed: 200)
-                  let sequence = SKAction.sequence([move, .removeFromParent()]) //This "sequence" is critical because it removes the bullet from parent with the function ".removeFromParent()" once the "move" function is complete
-                  n.run(sequence)
-              }
+              n.position = player!.anchorPoint //TODO: CRITICAL NOTE, MUST USE "anchorPoint" to generate proper player position when launching a sprite node from the player's "position"
+              //n.position = CGPoint(x:-100, y: -100)
+              //n.position = CGPoint(x: player!.position.x-30, y: player!.position.y+150)
+              //CRITICAL DEBUGGING ISSUE ENCOUNTERED -- SETTING THIS POSITION TO 0,0 USES THE ORIGINAL SCREEN 0,0 WHICH IS THE MIDDLE.  HOWEVER, THE 0,0 OF AN SKSHAPENODE IS THE BOTTOM-LEFT CORNER OF THE SCREEN.  NEED TO FIND A WAY TO LINK THESE TWO TO THE SAME POINT OF REFERENCE!!!
+              print(pos)
+              print(player!.anchorPoint)//anchor point is (0.5,0.5).  This seems to be a basis point for where the player position is derived?
+
+              n.strokeColor = SKColor.green
+              n.fillColor = SKColor.green
+              
+              self.addChild(n)
+              
+              //MyNotes: Next, create a movement path for the bullet object from the player to offscreen
+              //This section of code creates a path object and makes n move along the path
+              let bulletpath = UIBezierPath()
+              
+               bulletpath.move(to: player!.position)
+              //path.addLine(to: CGPoint(x:0,y:200)) //bullet shoots toward center.
+              bulletpath.addLine(to: CGPoint(x:player!.position.x,y:player!.position.y+1500) ) //Added 1500 because that is a significant offscreen distance for the bullet to travel.
+              let move = SKAction.follow(bulletpath.cgPath, asOffset: true, orientToPath: true, speed: 500)
+              let sequence = SKAction.sequence([move, .removeFromParent()]) //This "sequence" is critical because it removes the bullet from parent with the function ".removeFromParent()" once the "move" function is complete
+              n.run(sequence)
+      }
         
        
         
